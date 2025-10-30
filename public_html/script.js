5bcaefa47bc0a6c80ab65e48ae096e9660e2f209
Florian Garcia Soto <floriangarciasoto@outlook.fr>
2025-10-30 15:19:55 +0100

    Ajout d'un menu burger pour mobile avec apparition animée au clic sur le bouton burger

--- public_html/script.js
+++ public_html/script.js
@@ -1,5 +1,12 @@
+/*
+=========================================================================
+|| DARK MODE
+=========================================================================
+*/
+
 // Récuperation du mode par localStorage
 let darkMode = localStorage.darkMode;
+
 // Si le mode dans le localStorage n'est pas définit, on prend une valeur par défaut (ici thème clair)
 if (darkMode === undefined) darkMode = false;
 
@@ -7,6 +14,7 @@ if (darkMode === undefined) darkMode = false;
 // Il faut donc convertir la valeur en booléen afin de pouvoir correctement l'utiliser par la suite.
 if (darkMode === "false") darkMode = false;
 if (darkMode === "true") darkMode = true;
+
 
 /**
  * Inverse la variable représentant le thème de la page
@@ -43,6 +51,7 @@ function applyMode() {
     }
 }
 
+
 // On applique d'abord le thème laissé par localStorage ou celui par défaut
 applyMode();
 
@@ -60,3 +69,50 @@ document.addEventListener('click', e => {
     }
 });
 
+
+
+/*
+=========================================================================
+|| MENU BURGER
+=========================================================================
+*/
+
+// On sélectionne le menu burger sur mobile
+let burgerMenu = document.getElementById("burger-menu");
+// On sélectionne le bouton burger sur mobile
+let burgerButton = document.getElementById("burger-button");
+
+
+/**
+ * Fait apparaître ou disparaitre le menu burger sur mobile avec une animation
+ */
+function toogleBurgerMenu() {
+    // On sélectionne uniquement les classes du menu mobile
+    let burgerMenuClassList = burgerMenu.classList;
+    // On ajoute une classe qui va permettre d'imposer une durée lorsque
+    // l'on va faire un changement de style sur le menu, ici on va modifier sa hauteur
+    burgerMenuClassList.add("animate");
+
+    // La classe "opened" va donner une valeur arbitraire à la nav afin de pouvoir
+    // passer de 0 (valeur initiale) à quelque chose, ce qui permet de faire une transition
+    // entre ces deux hauteurs.
+    // Il suffit alors de switcher la présence de la classe.
+    if (burgerMenuClassList.contains("opened")) burgerMenuClassList.remove("opened");
+    else burgerMenuClassList.add("opened");
+
+    // Durant la transition, il est important de garder la classe "animate" afin qu'elle puisse
+    // permettre l'animation de transition, il ne faut alors pas l'enlever tout de suite,
+    // on donne un délai de 500 ms
+    setTimeout(()=>{
+        // Une fois que ce délai est terminé, on peut enfin retirer la classe
+        burgerMenuClassList.remove("animate");
+    },500);
+}
+
+
+// On ajoute l'évènement au clic sur le bouton burger afin d'appeler notre fonction
+// qui va le faire apparaître ou disparaître
+burgerButton.onclick = function() {
+    // Appel de la fonction
+    toogleBurgerMenu();
+}
